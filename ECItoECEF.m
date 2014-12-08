c439c8259b93e11e991443edb2067bd4504bd9d8
Geoffrey Card <gercunderscore4@gmail.com>
2014-12-08 02:48:51 -0500

    commented

--- ECItoECEF.m
+++ ECItoECEF.m
@@ -1,3 +1,8 @@
+% INPUT:
+%     [lat long h] is ECI latitude, longitude, and orbital radius in ° and m
+% OUTPUT:
+%     [lat long h] is ECEF latitude, longitude, and orbital radius in ° and m
+
 function [lat long h] = ECItoECEF(lat, long, h, DELTA_time)
 	long = long + [1:length(long)]'*DELTA_time*360/(24*3600);
 	long = mod(long + 180, 360) - 180;
